d4bde03df6fc991bea490c704aeb0c0c37078379
Aaqibhafeez Khan <githubaaqibhafeezkhan@gmail.com>
2026-08-13 12:23:25 +0530

    Clean up mock mode entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./mockApi.js";
 import "./index.css";
 import App from "./App.jsx";
 import { createMockFetch } from "./mockApi.js";
